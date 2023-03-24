7f425c17b27c66ab872fbe48728e43f05f744882
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-03-24 06:04:38 +0430

    Removed dtype

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch
 model_id = "mann-e/mann-e_4_rev-1-3"
 scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
 
-pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler)
 pipe = pipe.to("cpu")
 
 def dummy(images, **kwargs): 
