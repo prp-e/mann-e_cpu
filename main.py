197820d37bc980c514407436cd095c72e29a8147
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-03-24 06:06:41 +0430

    almost done, ready to test

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ prompt = "a historical city in an (((island))), view from the sea, cyberpunk, fl
 negative_prompt = "low quality, blurry"
 width = 512
 height = 512 
+
+print(f'prompt is {prompt}')
+image = pipe(prompt=prompt, negative_prompt=negative_prompt, num_inference_steps=100, width=width, height=height, guidance_scale=10).images[0]
+image.save("output.png")
