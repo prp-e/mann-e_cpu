dc86e716fa0026062ae30b961e6d14b522183d3c
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-03-24 05:43:53 +0430

    added 4.1.3 model

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 from diffusers import StableDiffusionPipeline, EulerAncestralDiscreteScheduler
 import torch
+
+model_id = "mann-e/mann-e_4_rev-1-3"
+scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
