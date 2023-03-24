63a930858234112a79ea0d439fa6755543eda1f8
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-03-24 05:44:39 +0430

    added pipeline

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import torch
 
 model_id = "mann-e/mann-e_4_rev-1-3"
 scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+pipe = pipe.to("cpu")
+
+def dummy(images, **kwargs): 
+    return images, False 
+
+pipe.safety_checker = dummy
